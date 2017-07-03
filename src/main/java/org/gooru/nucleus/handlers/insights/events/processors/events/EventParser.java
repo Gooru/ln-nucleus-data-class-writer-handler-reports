c690ef39537abb5f4a3aa678032541ff36676066
gooru <mukul@gooru.org>
2017-07-03 10:56:34 +0530

    Revert "Merge pull request #37 from daniel1028/nile-develop"
    This reverts commit 85f9b85d2f44c4218392c555f00253824fbaffba, reversing
    changes made to 90c196fa45e358024b75f5bf6625b1482cd9e48b.

--- src/main/java/org/gooru/nucleus/handlers/insights/events/processors/events/EventParser.java
+++ src/main/java/org/gooru/nucleus/handlers/insights/events/processors/events/EventParser.java
@@ -110,14 +110,10 @@ public final class EventParser {
 		
 		private String collectionSubType;
 
-    private double maxScore;		
+        public double maxScore;		
 
-    private long pathId;
+        public long pathId;
     
-    private String sourceId;
-
-    private String accessToken;
-
 		public String getEventId() {
 			return eventId;
 		}
@@ -593,9 +589,6 @@ public final class EventParser {
 				this.reaction = context.containsKey(EventConstants.REACTION_TYPE) ? context.getLong(EventConstants.REACTION_TYPE) : 0;
 				this.contentSource = context.containsKey(EventConstants.CONTENT_SOURCE) ? context.getString(EventConstants.CONTENT_SOURCE) : null;
 
-	      this.sourceId = (payLoadObject.containsKey("sourceId") ? null : payLoadObject.getString("sourceId"));
-        this.accessToken = (payLoadObject.containsKey("accessToken") ? null : payLoadObject.getString("accessToken"));
-
 				if (this.eventName.equals(EventConstants.COLLECTION_PLAY)){
 					LOGGER.debug("Inside Collection.Play");
 					processCollectionPlayEvents();
@@ -651,22 +644,6 @@ public final class EventParser {
 								
 			}
 		}
-
-    public String getSourceId() {
-      return sourceId;
-    }
-
-    public void setSourceId(String sourceId) {
-      this.sourceId = sourceId;
-    }
-
-    public String getAccessToken() {
-      return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-      this.accessToken = accessToken;
-    }
 
 	} // End Class
 
